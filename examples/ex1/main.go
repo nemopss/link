7962b8da8488c00f1a324c556ca7315b75c97b2e
nemopss <alexey_gladilin@mail.ru>
2024-06-03 23:36:32 +0300

    Initial commit

---
+++ examples/ex1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nemopss/link"
+)
+
+var exampleHTML = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHTML)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nemopss/link
+
+go 1.22.3
