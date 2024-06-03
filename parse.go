fd7c9582f6303196c7fe0a60bac29010e5ad95d8
nemopss <alexey_gladilin@mail.ru>
2024-06-04 00:39:36 +0300

    Added links building
    TODO: Parse the text from links

--- parse.go
+++ parse.go
@@ -1,7 +1,6 @@
 package link
 
 import (
-	"fmt"
 	"io"
 
 	"golang.org/x/net/html"
@@ -19,10 +18,23 @@ func Parse(r io.Reader) ([]Link, error) {
 		return nil, err
 	}
 	nodes := linkNodes(doc)
+	var links []Link
 	for _, node := range nodes {
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
-	return nil, nil
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = "TODO: Parse the text"
+	return ret
 }
 
 func linkNodes(n *html.Node) []*html.Node {
